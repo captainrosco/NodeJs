bbda920aba99e8f651b5fb0140a488a140b88734
Daron Short <daron.short@hotmail.com>
2020-01-13 21:32:49 -0600

    Creating a basic route.

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  res.setHeader("Content-Type", "text/html");
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>The Server Lives!</title></head>");
+    res.write("<body><h1>Server Lives!</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(5000);
