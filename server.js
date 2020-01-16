8161ae3f9347b0ec79017fa13e2bcd758073d5a5
Daron Short <daron.short@hotmail.com>
2020-01-15 19:16:50 -0600

    Added Views for Routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,9 +8,13 @@ const shopRoutes = require(`./routes/shop`);
 const app = express();
 
 //Middleware
-app.use(adminRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(5000);
