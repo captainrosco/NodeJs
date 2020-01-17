89031f184f69df82fc5eb32fd3d338cce26fd552
Daron Short <daron.short@hotmail.com>
2020-01-16 21:51:54 -0600

    Implement partial layouts with ejs

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const products = [];
 
 router.get(`/add-product`, (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.render("add-product");
 });
 
 router.post(`/add-product`, (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const adminData = require("./admin");
 
 router.get(`/`, (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products });
 });
 
 module.exports = router;
