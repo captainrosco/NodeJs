a11cb198dbf02677bacb72b691717c669314ce2c
Daron Short <daron.short@hotmail.com>
2020-01-17 19:27:15 -0600

    Updated Routes to use controllers for MVC structure

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const adminData = require(`./routes/admin`);
+const errorController = require("./controllers/error");
+const adminRoutes = require(`./routes/admin`);
 const shopRoutes = require(`./routes/shop`);
 
 const app = express();
@@ -15,11 +15,9 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404");
-});
+app.use(errorController.get404);
 
 app.listen(5000);
