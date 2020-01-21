595ac60d7709cf88fe16399e1eea05f86b4fc670
Daron Short <daron.short@hotmail.com>
2020-01-20 20:17:44 -0600

    Added Product/id route and handler methods in controller and Product

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const shopRoutes = require(`./routes/shop`);
 const app = express();
 
 //config
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 //Middleware
